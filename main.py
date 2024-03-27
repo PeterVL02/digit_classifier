efb2bb07168fa99c52d1d06a540aef3a8f746524
Peter <p.vesterenglarsen@gmail.com>
2024-03-27 02:11:09 +0100

    (single) k-fold cross-validation implemented

--- main.py
+++ main.py
@@ -49,63 +49,81 @@ if __name__ == '__main__':
     digits = datasets.load_digits()
     ims, labs = digits.images, digits.target
 
-    X_train, X_test, y_train, y_test = train_test_split(ims, labs, test_size=0.2, random_state=42)
+    ens_rolling_acc = []
+    lin_rolling_acc = []
+    conv_rolling_acc = []
+    svm_rolling_acc = []
+    gnb_rolling_acc = []
 
-    ## Reshape data for SciKit models
-    n_samples, width, height = X_train.shape
-    X_train_reshaped = X_train.reshape((n_samples, width * height))
+    for X_train, X_test, y_train, y_test in k_fold_cross_validation(ims, labs, k=5):
 
-    n_samples = X_test.shape[0]
-    X_test_reshaped = X_test.reshape((n_samples, width * height))
+        ## Reshape data for SciKit models
+        n_samples, width, height = X_train.shape
+        X_train_reshaped = X_train.reshape((n_samples, width * height))
 
-    ## Boring stuff some dude from YouTube did;
-    svmmodel = SVC(kernel='linear')
-    svmmodel.fit(X_train_reshaped, y_train)
-    print('Support Vector Classifier model accuracy:',SVMacc:=svmmodel.score(X_test_reshaped, y_test))
+        n_samples = X_test.shape[0]
+        X_test_reshaped = X_test.reshape((n_samples, width * height))
 
-    ## Boring stuff that i'll do:
+        ## Boring stuff some dude from YouTube did;
+        svmmodel = SVC(kernel='linear')
+        svmmodel.fit(X_train_reshaped, y_train)
+        print('Support Vector Classifier model accuracy:',SVMacc:=svmmodel.score(X_test_reshaped, y_test))
 
-    gnb = GaussianNB()
-    gnb.fit(X_train_reshaped, y_train)
-    print('Naïve Bayes model accuracy:',GNBacc:=gnb.score(X_test_reshaped, y_test))
+        ## Boring stuff that i'll do:
 
-    ## Not nearly as fun as making your own model
+        gnb = GaussianNB()
+        gnb.fit(X_train_reshaped, y_train)
+        print('Naïve Bayes model accuracy:',GNBacc:=gnb.score(X_test_reshaped, y_test))
 
-
-    ## Define number of epochs
-    rttL, rttC = 3, 8
-
-    ## Instantiate trainers for each model
-    trainerL = Trainer(Linear_Net, X_train, y_train, X_test, y_test)
-    trainerC = Trainer(Conv_Net, X_train, y_train, X_test, y_test)
-
-    ## Train models and get training loss
-    Lloss = trainerL.train(epochs=rttL, plot_loss=True)
-    Closs = trainerC.train(epochs=rttC, plot_loss=True)
-
-    ## Test models and get accuracy
-    accL = trainerL.test()
-    accC = trainerC.test()
-
-    accENS = ensemble(Lin=trainerL, Conv=trainerC, X_test=X_test, y_test=y_test)
+        ## Not nearly as fun as making your own model
 
 
-    ## Print results
-    print('Linear Net accuracy:',accL)
-    print('Conv Net accuracy:',accC)
-    print('Ensemble Net accuracy:',accENS)
-    print('Support Vector Classifier accuracy:',SVMacc)
-    print('Naïve Bayes accuracy:',GNBacc)
-    print('-------------------------------------------------------------')
-    print('Conv Net improvement:',f'{(accC-SVMacc)*100:.2f}%')
-    print('Linear Net improvement:',f'{(accL-SVMacc)*100:.2f}%')
-    print('Ensemble Net improvement:',f'{(accENS-SVMacc)*100:.2f}%')
+        ## Define number of epochs
+        rttL, rttC = 3, 8
 
-    if accC > max(SVMacc, GNBacc) or accL > max(SVMacc, GNBacc) or accENS > max(SVMacc, GNBacc):
-        print('We beat the SciKit models! At least those I have created...')
-        
+        ## Instantiate trainers for each model
+        trainerL = Trainer(Linear_Net, X_train, y_train, X_test, y_test)
+        trainerC = Trainer(Conv_Net, X_train, y_train, X_test, y_test)
 
-    ## Plot loss
-    plot_loss(Lloss, 'Linear Net')
-    plot_loss(Closs, 'Conv Net')
-    print('-------------------------------------------------------------')
+        ## Train models and get training loss
+        Lloss = trainerL.train(epochs=rttL, plot_loss=True)
+        Closs = trainerC.train(epochs=rttC, plot_loss=True)
+
+        ## Test models and get accuracy
+        accL = trainerL.test()
+        accC = trainerC.test()
+
+        accENS = ensemble(Lin=trainerL, Conv=trainerC, X_test=X_test, y_test=y_test)
+
+
+        ## Print results
+        print('Linear Net accuracy:',accL)
+        print('Conv Net accuracy:',accC)
+        print('Ensemble Net accuracy:',accENS)
+        print('Support Vector Classifier accuracy:',SVMacc)
+        print('Naïve Bayes accuracy:',GNBacc)
+        print('-------------------------------------------------------------')
+        print('Conv Net improvement:',f'{(accC-SVMacc)*100:.2f}%')
+        print('Linear Net improvement:',f'{(accL-SVMacc)*100:.2f}%')
+        print('Ensemble Net improvement:',f'{(accENS-SVMacc)*100:.2f}%')
+
+        if accC > max(SVMacc, GNBacc) or accL > max(SVMacc, GNBacc) or accENS > max(SVMacc, GNBacc):
+            print('We beat the SciKit models! At least those I have created...')
+            
+
+        ## Plot loss
+        # plot_loss(Lloss, 'Linear Net')
+        # plot_loss(Closs, 'Conv Net')
+
+        print('-------------------------------------------------------------')
+        ens_rolling_acc.append(accENS)
+        lin_rolling_acc.append(accL)
+        conv_rolling_acc.append(accC)
+        svm_rolling_acc.append(SVMacc)
+        gnb_rolling_acc.append(GNBacc)
+
+    print('Ensemble Rolling Accuracy:',np.mean(ens_rolling_acc))
+    print('Linear Rolling Accuracy:',np.mean(lin_rolling_acc))
+    print('Conv Rolling Accuracy:',np.mean(conv_rolling_acc))
+    print('SVM Rolling Accuracy:',np.mean(svm_rolling_acc))
+    print('GNB Rolling Accuracy:',np.mean(gnb_rolling_acc))
